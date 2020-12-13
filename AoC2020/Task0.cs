cafa9a30e25a4047bbb49ca126f6c07fd87a9a8a
Antonina Nevstedt <tonyaaaaa@gmail.com>
2020-12-13 01:39:13 +0100

    Comment Task 0

--- AoC2020/Task0.cs
+++ AoC2020/Task0.cs
@@ -7,6 +7,9 @@ namespace AoC2020
 {
     class Task0
     {
+        /*
+         * The input file 0.txt contains a list of numbers. Let's find the sum of the numbers.
+         */
         public static void Part1()
         {
             var lines = File.ReadAllLines(@"..\..\..\input\0.txt");
@@ -18,10 +21,13 @@ namespace AoC2020
             Console.WriteLine(sum);
         }
 
+        /*
+         * And now let's find the largest number.
+         */
         public static void Part2()
         {
             var lines = File.ReadAllLines(@"..\..\..\input\0.txt");
-            int max = 0;
+            int max = int.MinValue;
             foreach (string line in lines)
             {
                 int number = int.Parse(line);
