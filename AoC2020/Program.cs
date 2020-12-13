35c0bf4da873f67d1e579579686021ee588ba6d6
Antonina Nevstedt <tonyaaaaa@gmail.com>
2020-12-13 01:03:28 +0100

    Add project files.

---
+++ AoC2020/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AoC2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task0.Part1();
+            Task0.Part2();
+        }
+    }
+}
---
+++ AoC2020/Task0.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AoC2020
+{
+    class Task0
+    {
+        public static void Part1()
+        {
+            var lines = File.ReadAllLines(@"..\..\..\input\0.txt");
+            int sum = 0;
+            foreach (string line in lines)
+            {
+                sum += int.Parse(line);
+            }
+            Console.WriteLine(sum);
+        }
+
+        public static void Part2()
+        {
+            var lines = File.ReadAllLines(@"..\..\..\input\0.txt");
+            int max = 0;
+            foreach (string line in lines)
+            {
+                int number = int.Parse(line);
+                if (number > max) max = number;
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
